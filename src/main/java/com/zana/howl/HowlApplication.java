f7a0c77c8220e4df71b824a0715e2078dd99ed25
zana-a <zana200@icloud.com>
2023-09-10 16:30:40 +0100

    feat: init spring boot

---
+++ src/main/java/com/zana/howl/HowlApplication.java
@@ -0,0 +1,11 @@
+package com.zana.howl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HowlApplication.class, args);
+    }
+}
